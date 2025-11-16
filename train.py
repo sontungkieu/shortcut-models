f74371096159496990ac42ab834b36a542f4491e
Kieu Son Tung <kieusontung8@gmail.com>
2025-11-16 23:34:35 +0700

    .

--- train.py
+++ train.py
@@ -73,10 +73,6 @@ model_config = ml_collections.ConfigDict({
 })
 
 wandb_config = default_wandb_config()
-wandb_config.update({
-    'project': 'shortcut',
-    'name': 'shortcut_{dataset_name}_{FLAGS.git_branch}_{FLAGS.machine}',
-})
 config_flags.DEFINE_config_dict('wandb', wandb_config, lock_config=False)
 config_flags.DEFINE_config_dict('model', model_config, lock_config=False)
 ##############################################
@@ -86,6 +82,10 @@ config_flags.DEFINE_config_dict('model', model_config, lock_config=False)
 
 def main(_):
 
+    wandb_config.update({
+        'project': 'shortcut',
+        'name': 'shortcut_{dataset_name}_{FLAGS.git_branch}_{FLAGS.machine}',
+    })
     np.random.seed(FLAGS.seed)
     print("Using devices", jax.local_devices())
     device_count = len(jax.local_devices())
